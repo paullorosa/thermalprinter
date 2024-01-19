ed7695fde9eea2b8d4f12799ef328fd5c84d87d7
paullorosa <rosa.paulo40@gmail.com>
2024-01-19 13:03:14 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Printer } from '@awesome-cordova-plugins/printer/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Printer],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Printer } from '@awesome-cordova-plugins/printer/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+
+export class HomePage {
+toprint: string = "Hello World";
+
+  constructor(private imprimir: Printer) {}
+
+  printNow(){
+    console.log("toprint", this.toprint);
+    
+    this.imprimir.print(this.toprint);
+  }
 
 }
