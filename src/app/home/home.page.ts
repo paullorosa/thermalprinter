c6963dc312cc3a7378b21cd81535109f9d469b6d
paullorosa <rosa.paulo40@gmail.com>
2024-01-22 12:13:30 +0200

    Impresao com fonte number 12

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,49 @@
 import { Component } from '@angular/core';
 import { Printer } from '@awesome-cordova-plugins/printer/ngx';
+
+// Declare manualmente o tipo FontOptions
+interface FontOptions {
+  align: 'left' | 'center' | 'right' | 'justified';
+  bold: boolean;
+  color: string;
+  italic: boolean;
+  size: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
-
 export class HomePage {
-toprint: string = "Hello World";
+  toprint: string = "Hello World";
 
   constructor(private imprimir: Printer) {}
 
-  printNow(){
-    console.log("toprint", this.toprint);
-    
-    this.imprimir.print(this.toprint);
-  }
+  printNow() {
+    let fontOpts: FontOptions = {
+      align: 'left', // ou 'center', 'right', 'justified'
+      bold: false,
+      color: 'black',
+      italic: false,
+      size: 12,
+      name: 'Times New Roman',
+    };
 
+    this.imprimir.print(
+      this.toprint,
+      {
+        name: 'MyDocument',
+        duplex: true,
+        orientation: 'portrait',
+        monochrome: true,
+        font: fontOpts,
+      },
+    ).then(() => {
+      console.log('Impressão concluída com sucesso.');
+    }).catch(err => {
+      console.error('Erro ao tentar imprimir:', err);
+    });
+  }
 }
